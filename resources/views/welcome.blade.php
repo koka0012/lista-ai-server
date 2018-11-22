7086e29a9e4e95a3d7018578d0e50203f5fc8d23
Augusto Chmieleski <augustocezarchmieleski@gmail.com>
2018-11-22 15:52:44 -0300

    feat(augusto) configured laradock

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Augusto
                 </div>
 
                 <div class="links">
