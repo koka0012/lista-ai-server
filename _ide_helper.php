b0b2b66c2cbb14c1d45f5c2efb09200068d98360
Augusto Chmieleski <augustocezarchmieleski@gmail.com>
2018-11-23 15:34:33 -0300

    inicio a criação do codigo

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.14 on 2018-11-23 17:03:04.
+ * Generated for Laravel 5.7.14 on 2018-11-23 18:31:59.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
