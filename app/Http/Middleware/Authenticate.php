cf0bfd0c4970ed6ac8118cf307c862ea290b9b41
Augusto Chmieleski <augustocezarchmieleski@gmail.com>
2018-11-23 16:31:53 -0300

    removed some migrations

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            Redirect::to('login')->withErrors(['message' => 'É necessário fazer o login']);
+            \Redirect::to('login')->withErrors(['message' => 'É necessário fazer o login']);
         }
     }
 }
